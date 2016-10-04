b9bdd9683f3d13650697ff8ee21c8a1ef41dcec1
Hello Wordovic <tf.2.98@mail.ru>
2016-10-05 01:09:49 +0300

    Ready to push.

--- logInTest/app/src/main/java/com/l/MainActivity.java
+++ logInTest/app/src/main/java/com/l/MainActivity.java
@@ -2,27 +2,42 @@ package com.l;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.l.logintest.R;
 
 public class MainActivity extends Activity {
 Button buttonNullActivity;
     TextView view ;
+    SharedPreferences sharedPreferences = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonNullActivity = (Button) findViewById(R.id.buttonNullActivity);
         view = (TextView) findViewById(R.id.view);
+        sharedPreferences = getSharedPreferences("com.l", MODE_PRIVATE);
         buttonNullActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, FirstActivity.class));
+                startActivity(getNextIntent());
             }
         });
+    }
+    private Intent getNextIntent(){
+        if(!sharedPreferences.getBoolean("wasInSix", false)){
+            sharedPreferences.edit().putBoolean("wentFromOneDirectlyToSix", false).commit();
+            return new Intent(MainActivity.this, FirstActivity.class);
+        } else {
+            sharedPreferences.edit().putBoolean("wentFromOneDirectlyToSix", true).commit();
+            return new Intent(MainActivity.this, SixActivity.class);
+        }
     }
 }
--- logInTest/app/src/main/java/com/l/SixActivity.java
+++ logInTest/app/src/main/java/com/l/SixActivity.java
@@ -1,15 +1,27 @@
 package com.l;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.l.logintest.R;
 
 public class SixActivity extends AppCompatActivity {
-
+    TextView view;
+    SharedPreferences sharedPreferences = null;
+    String textAgain = "Welcome again, mister. You're already been here.",
+            textFirst = "Hello. It's seems to see you first time here, mister.";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_six);
+        view = (TextView)findViewById(R.id.sixView);
+        sharedPreferences = getSharedPreferences("com.l", MODE_PRIVATE);
+        if(sharedPreferences.getBoolean("wentFromOneDirectlyToSix", true))
+            view.setText(textAgain);
+        else
+            view.setText(textFirst);
+
     }
 }
