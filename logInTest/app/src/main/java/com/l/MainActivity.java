75789b1b00ba79191328dd39d51bd505196a8cf6
Hello Wordovic <tf.2.98@mail.ru>
2016-10-04 23:10:23 +0300

    sm chngs

--- logInTest/app/src/main/java/com/l/MainActivity.java
+++ logInTest/app/src/main/java/com/l/MainActivity.java
@@ -2,6 +2,7 @@ package com.l;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +11,10 @@ import android.widget.TextView;
 import com.l.logintest.R;
 
 public class MainActivity extends Activity {
-Button buttonNullActivity;
+    Button buttonNullActivity;
     TextView view ;
+    SharedPreferences savedPreferences;
+    final boolean wasOnLast = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +27,9 @@ Button buttonNullActivity;
                 startActivity(new Intent(MainActivity.this, FirstActivity.class));
             }
         });
+        private Intent beforeCreate(){
+        savedPreferences = getSharedPreferences("wasOnLast", MODE_PRIVATE);
+        if()
+    }
     }
 }
